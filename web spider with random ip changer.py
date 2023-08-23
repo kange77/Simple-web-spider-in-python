e6e1f39c2725f8bfae83ac8521ef223f5e0efa42
kange <kangeken213@gmail.com>
2023-08-23 12:37:52 +0300

    Update web spider with random ip changer.py

--- web spider with random ip changer.py
+++ web spider with random ip changer.py
@@ -2,19 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-def get_random_ip():
-    # Generate a random IP address
-    ip = ".".join([str(random.randint(0, 255)) for _ in range(4)])
-    return ip
+class WebCrawler:
+    def __init__(self):
+        self.base_url = "http://www.example.com"
 
-def crawl(url: object) -> object:
-    # Use a random IP address to make the request
-    headers = {"X-Forwarded-For": get_random_ip()}
-    source_code = requests.get(url, headers=headers)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text)
+    def get_random_ip(self):
+        # Generate a random IP address
+        ip = ".".join([str(random.randint(0, 255)) for _ in range(4)])
+        return ip
 
-    # Print the body of the page
-    print(soup.body)
-# Enter website to be crawled here
-crawl("http://www.example.com")
+    def crawl(self, url):
+        try:
+            # Use a random IP address to make the request
+            headers = {"X-Forwarded-For": self.get_random_ip()}
+            source_code = requests.get(url, headers=headers)
+            plain_text = source_code.text
+            soup = BeautifulSoup(plain_text, "html.parser")
+
+            # Print page details
+            page_title = soup.title.text.strip()
+            print(f"Page Title: {page_title}\n")
+            self.print_paragraphs(soup.find_all("p"))
+        except requests.RequestException as req_err:
+            print("Request Error:", req_err)
+        except Exception as e:
+            print("An error occurred:", e)
+
+    def print_paragraphs(self, paragraphs):
+        if paragraphs:
+            print("Page Summary:")
+            for idx, paragraph in enumerate(paragraphs, start=1):
+                text = paragraph.get_text().strip()
+                print(f"{idx}. {text}\n")
+
+if __name__ == "__main__":
+    web_crawler = WebCrawler()
+    web_crawler.crawl(web_crawler.base_url)
